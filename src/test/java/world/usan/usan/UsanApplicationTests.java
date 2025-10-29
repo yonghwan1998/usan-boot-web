b81c65cb1bf3123f3ccf621f9adda981d8715461
yongss <bbl737898@gmail.com>
2025-10-29 09:47:55 +0900

    initial commit

---
+++ src/test/java/world/usan/usan/UsanApplicationTests.java
@@ -0,0 +1,13 @@
+package world.usan.usan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
