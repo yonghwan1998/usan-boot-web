b1f37acbed5b9dcd73602c6e7ed66cdcaece7492
yongss <bbl737898@gmail.com>
2025-10-29 15:43:52 +0900

    test: 웹 테스트

---
+++ src/main/java/world/usan/usan/controller/TestController.java
@@ -0,0 +1,12 @@
+package world.usan.usan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping
+    public String test(){
+        return "test";
+    }
+}
