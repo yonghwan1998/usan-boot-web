48a3205bae4197474455056107a6fa69c047326f
yongss <bbl737898@gmail.com>
2025-10-30 16:16:00 +0900

    feat: 네이버 지도 api 추가
    - .env에 nave api key 추가 필요
    - /map 경로에 네이버 지도 추가
    - application.yml에 .env 파일 내용 로드하는 코드 추가

--- src/main/java/world/usan/usan/controller/MapController.java
+++ src/main/java/world/usan/usan/controller/MapController.java
@@ -1,13 +1,19 @@
 package world.usan.usan.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MapController {
 
+    @Value("${NAVER_MAP_CLIENT_ID}")
+    private String naverClientId;
+
     @RequestMapping("/map")
-    public String map() {
+    public String map(Model model) {
+        model.addAttribute("naverClientId", naverClientId);
         return "pages/map";
     }
 }
