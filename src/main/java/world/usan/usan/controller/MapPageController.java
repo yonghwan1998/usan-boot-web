321dd7462d89f0f7e843b9f67b60a9b45c0f65c6
yongss <bbl737898@gmail.com>
2025-11-22 08:38:30 +0900

    feat: 지도 페이지 이동 시 DB 읽어서 중개사 위치 뿌려주기
    - /map 페이지 매핑
    - 클러스터링 없이 일단 전부 뿌려주기

--- src/main/java/world/usan/usan/controller/MapPageController.java
+++ src/main/java/world/usan/usan/controller/MapPageController.java
@@ -4,18 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Map;
 
+@RequestMapping("/map")
 @Controller
-public class MapController {
+public class MapPageController {
 
     @Value("${NAVER_MAP_CLIENT_ID}")
     private String naverClientId;
 
-    @GetMapping("/map")
-    public String map(Model model) {
+    @GetMapping("")
+    public String mapPage(Model model) {
         model.addAttribute("naverClientId", naverClientId);
         return "pages/map";
     }
