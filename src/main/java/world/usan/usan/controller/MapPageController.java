4edb2d0c5dd6ce04eb054074e365987b5a3e247b
yongss <bbl737898@gmail.com>
2025-12-02 17:10:43 +0900

    fix: 바텀 네비게이션에서 model에서 가져온 값 넣기
    - 바텀 네비게이션에서 model에서 가져온 값 씀
    - 이에 따라 controller에서 임시 데이터 넣기

--- src/main/java/world/usan/usan/controller/MapPageController.java
+++ src/main/java/world/usan/usan/controller/MapPageController.java
@@ -1,5 +1,6 @@
 package world.usan.usan.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,8 +18,12 @@ public class MapPageController {
     private String naverClientId;
 
     @GetMapping("")
-    public String mapPage(Model model) {
+    public String mapPage(HttpSession session, Model model) {
         model.addAttribute("naverClientId", naverClientId);
+
+        boolean tempLogin = Boolean.TRUE.equals(session.getAttribute("tempLogin"));
+        model.addAttribute("tempLogin", tempLogin);
+
         return "pages/map";
     }
 }
