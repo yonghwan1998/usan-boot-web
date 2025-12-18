67a398e5fe4793e1583a2e9301cc9d59059b002c
yongss <bbl737898@gmail.com>
2025-12-18 15:11:35 +0900

    feat: /region/selector 페이지 허용

--- src/main/java/world/usan/usan/config/SecurityConfig.java
+++ src/main/java/world/usan/usan/config/SecurityConfig.java
@@ -18,7 +18,16 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/join", "/map", "/css/**", "/js/**", "/img/**").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/login",
+                                "/join",
+                                "/region/selector",
+                                "/map",
+                                "/css/**",
+                                "/js/**",
+                                "/img/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth -> oauth
