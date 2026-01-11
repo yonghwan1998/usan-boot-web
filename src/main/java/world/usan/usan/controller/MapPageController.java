829e7b011dc57f5c4f208f511edce45d203d23eb
yongss <bbl737898@gmail.com>
2026-01-11 22:06:57 +0900

    feat: 매물 발송하기 지도 페이지 매핑

--- src/main/java/world/usan/usan/controller/MapPageController.java
+++ src/main/java/world/usan/usan/controller/MapPageController.java
@@ -1,14 +1,10 @@
 package world.usan.usan.controller;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.Map;
 
 @RequestMapping("/map")
 @Controller
@@ -18,9 +14,16 @@ public class MapPageController {
     private String naverClientId;
 
     @GetMapping("")
-    public String mapPage(HttpSession session, Model model) {
+    public String mapPage(Model model) {
         model.addAttribute("naverClientId", naverClientId);
 
-        return "pages/map";
+        return "pages/map/map";
+    }
+
+    @GetMapping("/listings/share")
+    public String listingsSharePage(Model model) {
+        model.addAttribute("naverClientId", naverClientId);
+
+        return "pages/map/map-listings-share";
     }
 }
