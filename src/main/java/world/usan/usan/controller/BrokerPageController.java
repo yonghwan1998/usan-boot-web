fc574f5c8969376ad491e9293c0f5e0f79ee173a
yongss <bbl737898@gmail.com>
2026-01-20 10:59:43 +0900

    feat: 중개사 자세히 보기 페이지 퍼블리싱
    - 중개사 자세히 보기 페이지
    - 퍼블리싱
    - 공통 매물 보내기 버튼 생성

--- src/main/java/world/usan/usan/controller/BrokerPageController.java
+++ src/main/java/world/usan/usan/controller/BrokerPageController.java
@@ -1,6 +1,8 @@
 package world.usan.usan.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/broker")
 public class BrokerPageController {
 
+    @Value("${NAVER_MAP_CLIENT_ID}")
+    private String naverClientId;
+
     @GetMapping("/{brokerCode}")
-    public String brokerPage() {
+    public String brokerPage(Model model) {
+        model.addAttribute("naverClientId", naverClientId);
+
         return "pages/broker";
     }
 }
