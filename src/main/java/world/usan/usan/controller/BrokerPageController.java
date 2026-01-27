2c3c73c8398b6875e888eca1a2445d4f02ae59b2
yongss <bbl737898@gmail.com>
2026-01-27 19:44:15 +0900

    feat: 중개사 상세 페이지 취급 매물 노출
    - 해당 중개사가 취급하는 매물 전체 보여주기

--- src/main/java/world/usan/usan/controller/BrokerPageController.java
+++ src/main/java/world/usan/usan/controller/BrokerPageController.java
@@ -7,9 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import world.usan.usan.dto.BrokerPropertyTagDto;
+import world.usan.usan.entity.BrokerPropertyCount;
 import world.usan.usan.entity.CrawledBroker;
+import world.usan.usan.service.BrokerPropertyCountService;
 import world.usan.usan.service.CrawledBrokerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -21,15 +25,16 @@ public class BrokerPageController {
     private String naverClientId;
 
     private final CrawledBrokerService crawledBrokerService;
+    private final BrokerPropertyCountService brokerPropertyCountService;
 
     @GetMapping("/{brokerCode}")
     public String brokerPage(@PathVariable UUID brokerCode, Model model) {
 
-        // TODO(yongss): broker_property_count 테이블의 컬럼을 broker_info 테이블과 맞추기
-        // TODO(yongss): broker_info 테이블에서 데이터 가져오는 게 아니라 broker_property_count 테이블에서 데이터 가져오기
         CrawledBroker broker = crawledBrokerService.getBroker(brokerCode);
+        List<BrokerPropertyTagDto> tags = brokerPropertyCountService.getBrokerTagsAll(brokerCode);
 
         model.addAttribute("broker", broker);
+        model.addAttribute("tags", tags);
         model.addAttribute("naverClientId", naverClientId);
 
         return "pages/broker";
