2b44756770a816264c7215e1adb5718131c88995
yongss <bbl737898@gmail.com>
2026-02-16 18:33:17 +0900

    feat: 주소, 상세 주소가 동일한 step에서 이루어짐
    - 주소 입력 step
    - 주소, 상세 주소 선택이 동일한 step에서 진행

--- src/main/java/world/usan/usan/dto/ListingRequest.java
+++ src/main/java/world/usan/usan/dto/ListingRequest.java
@@ -1,12 +1,18 @@
 package world.usan.usan.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import world.usan.usan.entity.enums.ListingRole;
 import world.usan.usan.entity.enums.ListingType;
 
 public record ListingRequest(
-        @NotBlank ListingRole role,
-        @NotBlank ListingType type,
-        @NotBlank String address
+        @NotNull ListingRole role,
+        @NotNull ListingType type,
+        @NotBlank String addressName,
+        String roadAddress,
+        String jibunAddress,
+        @NotNull Double lat,
+        @NotNull Double lng,
+        String addressDetail
 ) {
 }
