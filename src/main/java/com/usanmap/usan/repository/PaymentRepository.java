7e89006bfab2c17ae685b2dcfba42c51a76d2db4
yongss <bbl737898@gmail.com>
2026-04-08 19:21:19 +0900

    fix: 크레딧 결제 내역 페이지 접근 오류 수정

--- src/main/java/com/usanmap/usan/repository/PaymentRepository.java
+++ src/main/java/com/usanmap/usan/repository/PaymentRepository.java
@@ -17,9 +17,9 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     Optional<Payment> findByCreditOrder(CreditOrder creditOrder);
 
-    @Query("SELECT p FROM Payment p JOIN p.creditOrder o WHERE o.member.id = :memberId ORDER BY p.requestedAt DESC")
+    @Query("SELECT p FROM Payment p JOIN FETCH p.creditOrder o WHERE o.member.id = :memberId ORDER BY p.requestedAt DESC")
     Page<Payment> findByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
-    @Query("SELECT p FROM Payment p JOIN p.creditOrder o WHERE o.member.id = :memberId AND p.paymentStatus = :status ORDER BY p.requestedAt DESC")
+    @Query("SELECT p FROM Payment p JOIN FETCH p.creditOrder o WHERE o.member.id = :memberId AND p.paymentStatus = :status ORDER BY p.requestedAt DESC")
     Page<Payment> findByMemberIdAndStatus(@Param("memberId") Long memberId, @Param("status") PaymentStatus status, Pageable pageable);
 }
