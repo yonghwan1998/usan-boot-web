8655943ddf253952bb0c98906bd048bf0a761a9c
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-08-08 11:03:24 +0900

    refactor: mark Toss and KCP PG providers as deprecated
    - 기존 Toss, KCP PG Provider를 더 이상 사용하지 않음을 명시

--- src/main/java/com/usanmap/usan/entity/enums/PgProvider.java
+++ src/main/java/com/usanmap/usan/entity/enums/PgProvider.java
@@ -1,7 +1,16 @@
 package com.usanmap.usan.entity.enums;
 
 public enum PgProvider {
+    /**
+     * @deprecated 기존 토스페이먼츠 연동은 삭제됨. 과거 결제 이력(DB) 호환을 위해서만 유지.
+     */
+    @Deprecated
     TOSS,
     HECTO,
-    BANK_TRANSFER
+    BANK_TRANSFER,
+    /**
+     * @deprecated 기존 NHN KCP 연동은 삭제됨. 과거 결제 이력(DB) 호환을 위해서만 유지.
+     */
+    @Deprecated
+    KCP
 }
